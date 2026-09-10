b35145768cddfaa9fe15685d058211f9e97969da
wherefore <wherefore0@gmail.com>
2026-09-10 14:01:59 -0400

    the dwarves enter
    there's a lil entrance and spawn animation

--- project/loose/dwarf.gd
+++ project/loose/dwarf.gd
@@ -4,10 +4,28 @@ extends CharacterBody2D
 
 var speed:float = 50.0
 
-func _physics_process(_delta: float) -> void:
-	navigation_agent_2d.target_position = get_global_mouse_position()
-	var next_path_position:Vector2 = navigation_agent_2d.get_next_path_position()
-	var new_velocity:Vector2 = global_position.direction_to(next_path_position)
+var spawn_in_animation_timer:float = 1.5
+var active:bool
+
+func _ready() -> void:
+	active = false
+	scale = Vector2.ZERO
 	
-	velocity = new_velocity * speed
-	move_and_slide()
+	spawn_in_animation()
+
+func _physics_process(_delta: float) -> void:
+	if active:
+		navigation_agent_2d.target_position = get_global_mouse_position()
+		var next_path_position:Vector2 = navigation_agent_2d.get_next_path_position()
+		var new_velocity:Vector2 = global_position.direction_to(next_path_position)
+		
+		velocity = new_velocity * speed
+		move_and_slide()
+
+func spawn_in_animation() -> void:
+	var tween:Tween = create_tween()
+	tween.set_trans(Tween.TRANS_BOUNCE)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "scale", Vector2(1,1), spawn_in_animation_timer)
+	await tween.finished
+	active = true
--- project/loose/dwarf_spawner.gd
+++ project/loose/dwarf_spawner.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var individual_dwarf:PackedScene
 
 func spawn_a_dwarf() -> void:
-	var new_dwarf:Node = individual_dwarf.instantiate()
+	var new_dwarf:Node2D = individual_dwarf.instantiate()
 	add_child(new_dwarf)
 
 func _unhandled_input(event: InputEvent) -> void:
